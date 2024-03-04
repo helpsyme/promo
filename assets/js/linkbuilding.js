d13cd811962fc8cdc23ef0f0e566d07783263bd7
PavelSh <58147088+ps33033@users.noreply.github.com>
2024-03-04 10:55:47 +0200

    upd

--- assets/js/linkbuilding.js
+++ assets/js/linkbuilding.js
@@ -4,6 +4,10 @@ var contentHTML = postContent.innerHTML;
 
 // Заменяем все вхождения "консультація психіатра онлайн" на "XXX"
 contentHTML = contentHTML.replaceAll("Консультація психіатра", "<a href='https://helpsy.me/'>Консультація психіатра</a>");
+contentHTML = contentHTML.replaceAll("Психіатр онлайн", "<a href='https://helpsy.me/'>Психіатр онлайн</a>");
+contentHTML = contentHTML.replaceAll("Психіатри онлайн", "<a href='https://helpsy.me/'>Психіатри онлайн</a>");
+contentHTML = contentHTML.replaceAll("консультації з психіатром онлайн", "<a href='https://helpsy.me/'>консультації з психіатром онлайн</a>");
+contentHTML = contentHTML.replaceAll("Психіатричні консультації онлайн", "<a href='https://helpsy.me/'>Психіатричні консультації онлайн</a>");
 
 // Обновляем текстовое содержимое элемента с замененными данными
 postContent.innerHTML = contentHTML;
