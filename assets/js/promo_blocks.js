cc7f34259aeb123f43270d781c2b7efd712892a0
PavelSh <58147088+ps33033@users.noreply.github.com>
2024-03-05 11:06:26 +0200

    upd

---
+++ assets/js/promo_blocks.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+      $("#promo_1").load("../promo/promo_1.html");
+      $("#promo_2").load("../promo/promo_2.html");
+      $("#promo_3").load("../promo/promo_3.html");
+      $("#promo_4").load("../promo/promo_4.html");
+      $("#promo_depression").load("../promo/promo_depression.html");
+      $("#ua_cities").load("ua_cities.html");
+      $("#promo_btn_1").load("../promo/promo_btn_1.html");
+      $("#promo_btn_2").load("../promo/promo_btn_2.html");
+      $("#promo_btn_3").load("../promo/promo_btn_3.html");
+      $("#ua_cities").load("ua_cities.html");
+      $("#links").load("links.html");
+      $("#top_menu").load("top_menu.html");
+      $("#footer_menu").load("footer_menu.html");
+      
+    });
